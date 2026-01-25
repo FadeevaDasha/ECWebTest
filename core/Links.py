9667f6141d88a3250570e0613482e0d1335a8089
FadeevaDasha <genkovna1999@gmail.com>
2026-01-25 17:49:20 +0300

    Небольшие правки

---
+++ core/Links.py
@@ -0,0 +1,3 @@
+class Links:
+
+    BASE_URL = 'https://auth.truvisibility.net/en?ReturnUrl=https%3A%2F%2Fecommerce.truvisibility.net'
--- tests/test_Login.py
+++ tests/test_Login.py
@@ -1,17 +1,17 @@
 import allure
 
+from core.Links import Links
 from core.BaseTest import browser
 from pages.BasePage import BasePageHelper
 from pages.LoginPage import LoginPageHelper
 
-BASE_URL = 'https://auth.truvisibility.net/en?ReturnUrl=https%3A%2F%2Fecommerce.truvisibility.net'
 EMAIL_TEXT = 'evgeniyfadeev310322@gmail.com'
 PASSWORD_TEXT = ':tyzAflttd22'
 
 @allure.suite('Проверка формы авторизации')
 @allure.title('Проверка авторизации при заполненной формы')
 def test_empty_login_and_password(browser):
-    BasePageHelper(browser).get_url(BASE_URL)
+    BasePageHelper(browser).get_url(Links.BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.input_email_field(EMAIL_TEXT)
     LoginPage.input_password_field(PASSWORD_TEXT)
