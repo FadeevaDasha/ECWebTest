4307ab795785d3564a35a7b0c68d17039be78b64
FadeevaDasha <genkovna1999@gmail.com>
2026-02-06 16:00:33 +0300

    Поменяла название переменных

--- tests/test_Login.py
+++ tests/test_Login.py
@@ -13,10 +13,10 @@ PASSWORD_TEXT = ':tyzAflttd22'
 @allure.title('Проверка авторизации при заполненной формы')
 def test_valid_login_and_password(browser):
     BasePageHelper(browser).get_url(Links.AUTH)
-    LoginPage = LoginPageHelper(browser)
-    LoginPage.input_email_field(EMAIL_TEXT)
-    LoginPage.input_password_field(PASSWORD_TEXT)
-    LoginPage.click_login()
+    login_page = LoginPageHelper(browser)
+    login_page.input_email_field(EMAIL_TEXT)
+    login_page.input_password_field(PASSWORD_TEXT)
+    login_page.click_login()
 
 
 @allure.suite('Проверка формы авторизации')
--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -9,30 +9,30 @@ class TestDashboard:
 
     @allure.title('Проверка отображения графика')
     def test_checking_graph_display(self, authorization, browser):
-        DashboardPage = DashboardPageHelper(browser)
-        DashboardPage.display_chart()
+        dashboard_page = DashboardPageHelper(browser)
+        dashboard_page.display_chart()
 
     @allure.title('Проверка отображения заголовка')
     def test_checking_title_display(self, authorization, browser):
-        DashboardPage = DashboardPageHelper(browser)
-        DashboardPage.display_title()
+        dashboard_page = DashboardPageHelper(browser)
+        dashboard_page.display_title()
 
     @allure.title('Проверка отображения статистики')
     def test_checking_statistics_display(self, authorization, browser):
-        DashboardPage = DashboardPageHelper(browser)
-        DashboardPage.display_statistics()
+        dashboard_page = DashboardPageHelper(browser)
+        dashboard_page.display_statistics()
 
     @allure.title('Проверка отображения топа продуктов')
     def test_checking_products_display(self, authorization, browser):
-        DashboardPage = DashboardPageHelper(browser)
-        DashboardPage.display_products()
+        dashboard_page = DashboardPageHelper(browser)
+        dashboard_page.display_products()
 
     @allure.title('Проверка отображения топа кастомеров')
     def test_checking_customers_display(self, authorization, browser):
-        DashboardPage = DashboardPageHelper(browser)
-        DashboardPage.display_customers()
+        dashboard_page = DashboardPageHelper(browser)
+        dashboard_page.display_customers()
 
     @allure.title('Проверка отображения упавших транзакций')
     def test_checking_failed_transactions_display(self, authorization, browser):
-        DashboardPage = DashboardPageHelper(browser)
-        DashboardPage.display_failed_transactions()
+        dashboard_page = DashboardPageHelper(browser)
+        dashboard_page.display_failed_transactions()
